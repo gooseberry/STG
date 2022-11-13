02c74463c2db347c929127ebb545f540b03432df
gooseberry <eric.desgroseilliers@gmail.com>
2022-11-13 12:00:13 -0500

    game loop and quit added

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 
 import pygame
 from pygame.locals import *
+
+pygame.init()
+
+
+# Game Loop
+while True:
+    
+    pygame.display.update()
+
+    # quit game
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit
