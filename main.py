444d6c34f4762c106fca74dcec9c4ca3f4a78409
gooseberry <eric.desgroseilliers@gmail.com>
2022-11-14 22:14:40 -0500

    cleanup

--- main.py
+++ main.py
@@ -45,16 +45,15 @@ class HUD():
 
     def update(self, player):
         self.speed = self.font.render("Speed: " + str(int(player.speed)), True, WHITE)
-        self.main_thrust = self.font.render("Thrust: " + str(int(player.main_thrust)), True, WHITE)
 
     def draw(self, surface):
         surface.blit(self.speed, (self.location_x, self.location_y))
-        surface.blit(self.main_thrust, (self.location_x, self.location_y + 25))
  
  
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
+
         super().__init__() 
 
         # Define the sprite image and size
@@ -64,21 +63,19 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (300, 600)
 
         # Set Maximum values for different speeds
-        self.MAX_AFTERBURNER = 200
-        self.MAX_SPEED = 120
         self.MAX_THRUST = 5
+        self.mass = 10
         
         # Set initial values
         self.speed = 0          # speed relative to the universe
+        self.main_thrust = 20
         self.v_thrust = 0       # vertical speed relative to the screen
         self.h_thrust = 0       # horizontal speed relative to the screen
         self.main_engines = False   # ship starts stopped
         
         # Accelaration values
-        self.main_thrust = 0    # How fast the ship accelerates relative to the universe
         self.thrusters = 0.4    # How fast the ship accelerates relative to the screen.
-        self.afterburners = 2   # How fast the afterburner accelerates the ship.
-        self.drag = 0.97        # How fast the drag will decelarate movement ( 1 = no drag )
+        self.drag = 0.98        # How fast the drag will decelarate movement ( 1 = no drag )
 
         # Load sound effects
         self.boing_sound = pygame.mixer.Sound("assets/sounds/boing.wav")
@@ -87,50 +84,47 @@ class Player(pygame.sprite.Sprite):
         pressed_keys = pygame.key.get_pressed()
         self.image = pygame.image.load("assets/img/player.png")
 
-        # Move up
-        if self.rect.top > (100):
-            if pressed_keys[K_UP] and self.v_thrust > -self.MAX_THRUST:
-                self.v_thrust -= self.thrusters
-        elif self.rect.top <= (100) and self.v_thrust < 0:
+        # Maneuvering thrusters
+        if pressed_keys[K_UP] and self.v_thrust > -self.MAX_THRUST:
+            self.v_thrust -= self.thrusters
+        if pressed_keys[K_DOWN] and self.v_thrust < self.MAX_THRUST:
+            self.v_thrust += self.thrusters
+        if pressed_keys[K_LEFT]:
+            self.image = pygame.image.load("assets/img/player_left.png")
+            if self.h_thrust > -self.MAX_THRUST:
+                self.h_thrust -= self.thrusters
+        if pressed_keys[K_RIGHT]:
+            self.image = pygame.image.load("assets/img/player_right.png")
+            if self.h_thrust < self.MAX_THRUST:
+                self.h_thrust += self.thrusters
+
+        # Bounding box for maneuveuring
+        if self.rect.top <= (100) and self.v_thrust < 0:
+            self.v_thrust = 0        
+        if self.rect.bottom >= (self.surface_height-100) and self.v_thrust > 0:
             self.v_thrust = 0
-
-        # Move down
-        if self.rect.bottom < (self.surface_height-100):
-            if pressed_keys[K_DOWN] and self.v_thrust < self.MAX_THRUST:
-                self.v_thrust += self.thrusters
-        elif self.rect.bottom >= (self.surface_height-100) and self.v_thrust > 0:
-            self.v_thrust = 0
-
-        # Move left
-        if self.rect.left > 0:
-            if pressed_keys[K_LEFT]:
-                self.image = pygame.image.load("assets/img/player_left.png")
-                if self.h_thrust > -self.MAX_THRUST:
-                    self.h_thrust -= self.thrusters
-        elif self.rect.left <= 0 and self.h_thrust < 0:
-            self.h_thrust = -self.h_thrust  # bounce if player hits screen edge
+        if self.rect.left <= 0 and self.h_thrust < 0:
+            self.h_thrust = -self.h_thrust  # bounce player off screen edge
             pygame.mixer.Sound.play(self.boing_sound)
-
-        # Move right
-        if self.rect.right < self.suface_width:        
-            if pressed_keys[K_RIGHT]:
-                self.image = pygame.image.load("assets/img/player_right.png")
-                if self.h_thrust < self.MAX_THRUST:
-                    self.h_thrust += self.thrusters
-        elif self.rect.right >= self.suface_width and self.h_thrust > 0:
-            self.h_thrust = -self.h_thrust  # bounce if player hits screen edge
+        if self.rect.right >= self.suface_width and self.h_thrust > 0:
+            self.h_thrust = -self.h_thrust  # bounce player off screen edge
             pygame.mixer.Sound.play(self.boing_sound)
-
-        # Engage Engine
-        if pressed_keys[K_w]:
-            self.main_engines = True
-
-        # Disengage Engine
-        if pressed_keys[K_s]:
-            self.main_engines = False
-            self.main_thrust = 0
 
         self.rect.move_ip(self.h_thrust, self.v_thrust)
+
+        # Main engines
+        if pressed_keys[K_w]:
+            self.main_engines = True
+        if pressed_keys[K_s]:
+            self.main_engines = False 
+
+        # Ship keeps accelerating until it reaches MAX_SPEED
+        if self.main_engines is True:
+            self.speed += self.main_thrust/self.mass
+
+        # Afterburners
+        if pressed_keys[K_LSHIFT] and self.main_engines is True:
+            self.speed += (self.main_thrust*2)/self.mass
 
         # Apply drag coefficient to all speeds relative to the screen.
         if self.h_thrust != 0:
@@ -140,19 +134,12 @@ class Player(pygame.sprite.Sprite):
         if self.speed != 0:
             self.speed *= self.drag
 
-        # Ship keeps accelerating until it reaches MAX_SPEED
-        if self.main_engines is True:
-            if self.main_thrust < self.MAX_THRUST:
-                self.main_thrust += self.thrusters/10
-            self.speed += self.main_thrust
-            if self.speed > self.MAX_SPEED:
-                self.speed = self.MAX_SPEED
-
     def draw(self, surface):
         surface.blit(self.image, self.rect)   
 
 
 class ScrollingBackground:
+
     def __init__(self, img_path):
         self.suface_width, self.surface_height = pygame.display.get_surface().get_size()
         self.img = pygame.image.load(img_path).convert_alpha()
@@ -169,8 +156,8 @@ class ScrollingBackground:
 
 
 class Scene:
-    def __init__(self):
 
+    def __init__(self):
         # Screen information
         self.screen_width = 600
         self.screen_height = 800
@@ -194,11 +181,13 @@ class Scene:
 
     def update(self):
         self.P1.update()
-        self.background.update(self.P1.speed)
-        self.texture.update(self.P1.speed*.9)
+        self.background.update(self.P1.speed*0.02)
+        self.texture.update(self.P1.speed*0.09)
         self.star_field_1.update(self.P1.speed*0.01)
         self.star_field_2.update(self.P1.speed*0.05)
         self.hud.update(self.P1)
+
+        self.drawScene()
 
 
 scene = Scene()
@@ -210,6 +199,5 @@ while True:
             pygame.quit()
             sys.exit()
 
-    scene.update()
-    scene.drawScene()     
+    scene.update()  
     FramePerSec.tick(FPS)
